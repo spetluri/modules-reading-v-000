93f9c06edd722c78b203bb144cda4c25f19e8e4e
Sai Petluri <spetluri.1@gmail.com>
2021-03-21 11:11:33 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,18 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  # extend MetaDancing
+  # include Dance
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  
+end
